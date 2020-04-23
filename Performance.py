24db9a00cbde8a05949e1ed84ad0e9de007e7de2
zzx <zzx_2015@buaa.edu.cn>
2020-04-23 12:29:07 -0400

    Update Performance.py

--- Performance.py
+++ Performance.py
@@ -1,6 +1,18 @@
+from mininet.log import setLogLevel, info
+from mininet.node import Controller
+from mininet.link import TCLink
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.node import RemoteController
+from mininet.cli import CLI
+import subprocess
+import sys
+import threading
+from concurrent.futures import thread
 class fatTree(Topo):
     "create fat tree topo with 4 core 8 aggregate 8 edge 16 hosts"
-    
     def build(self, n=4):
         K=4
         coreSet = {}
@@ -35,15 +47,44 @@ class fatTree(Topo):
         for i in range(0,K*K,1):
             self.addLink(edgeSet[i/2],hostSet[i],bw=10)
 
+def iperf_thread(net, src, dst):#cited from "lebiednik/ICNmininet" 
+    hostPair = [src, dst]
+    bandwidth = net.iperf(hostPair, seconds = 10)
+
 def perfTest():
     "test the fat tree"
     topo = fatTree(n=4)
     net = Mininet( topo=topo, controller=None)
     net.addController( 'ctr0', controller=RemoteController, ip='127.0.0.1', port=6633 ) 
     net.build()
+    net.waitConnected()
+    net.pingAll()
+    host={}
+    maxHost = 16
+    for y in range(0, maxHost):
+        hostName = 'h' +str(y)
+        host[y] = net.get(hostName)
+
+    for x in range(0, (maxHost/2)):
+        src = host[x]
+        dst = host[(maxHost-1)-x]
+        thread.start_new_thread(iperf_thread(net, dst, src))
+        thread.start_new_thread(iperf_thread(net, src, dst))
+    out= {}
+
+    for h in range(0, maxHost):
+        out[ host[h] ] = '/tmp/%s.out' % host[h].name
+        host[h].cmd( 'echo >', out[ host[h] ] )
+
+    packetSize=[50,100,150,200,300,450,600,750,900,1200]
+    for i in range (10):
+        host[h].cmdPrint('ping', host[h+1].IP(), '-s', packetSize[i], '>', out[ host[h] ], '&' )
+    
     CLI(net)
     net.stop()
+    subprocess.call(['mn', '-c'])
     pass
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    setLogLevel('info')
     perfTest()
